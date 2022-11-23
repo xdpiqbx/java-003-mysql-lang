ff8034a410b7d6f7bd411815c4dd252ef7d1ae2f
xdpiqbx <xdpiqbx@gmail.com>
2022-11-23 23:18:12 +0200

    added find_longest_project.sql

---
+++ HW_Megasoft/find_longest_project.sql
@@ -0,0 +1,23 @@
+SELECT
+    id AS name,
+    DATEDIFF(
+        MONTH,
+        CAST (start_date AS DATE),
+        CAST(finish_date AS DATE)
+    ) AS month_count
+    FROM project
+        WHERE DATEDIFF(
+                  MONTH,
+                  CAST (start_date AS DATE),
+                  CAST(finish_date AS DATE)
+              ) = (
+                  SELECT
+                      DATEDIFF(
+                          MONTH,
+                          CAST (start_date AS DATE),
+                          CAST(finish_date AS DATE)
+                      ) AS month_count
+                  FROM project
+                  ORDER BY month_count DESC
+                  LIMIT 1
+              )
