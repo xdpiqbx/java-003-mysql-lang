6d813ee6d35f58af26d950cf402be9666a2b04df
xdpiqbx <xdpiqbx@gmail.com>
2022-11-23 22:49:26 +0200

    added find_max_projects_client.sql done

--- HW_Megasoft/find_max_projects_client.sql
+++ HW_Megasoft/find_max_projects_client.sql
@@ -1,4 +1,11 @@
-SELECT client_id, COUNT(id) AS project_count
-    FROM project
-    GROUP BY client_id
--- in process
+SELECT name, COUNT(project.client_id) AS project_count
+    FROM client INNER JOIN project
+        ON client.id = project.client_id
+        GROUP BY name
+        HAVING COUNT(project.client_id) IN (
+            SELECT
+                COUNT(client_id),
+                FROM project
+                GROUP BY client_id
+                LIMIT 1
+        )
