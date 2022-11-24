9a3706c1f78ef5c81b6751b044ffe5568bf2d9fe
xdpiqbx <xdpiqbx@gmail.com>
2022-11-24 07:57:50 +0200

    find_youngest_eldest_workers.sql in process

--- HW_Megasoft/find_youngest_eldest_workers.sql
+++ HW_Megasoft/find_youngest_eldest_workers.sql
@@ -1,1 +1,14 @@
-SELECT * from project
+SELECT 'example' as TempField, name, birthday
+    FROM worker
+    WHERE CAST (birthday AS DATE) IN (
+        SELECT
+            MAX(CAST (birthday AS DATE))
+        FROM worker
+        UNION
+        SELECT
+            MIN(CAST (birthday AS DATE))
+        FROM worker
+    )
+
+
+-- CAST (birthday AS DATE)
